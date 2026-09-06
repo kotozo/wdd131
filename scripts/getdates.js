9e9b5db69d12e010fde5fd3422638b0679c7e51e
kotozo <toz20002@byui.edu>
2026-09-06 22:26:30 +0000

    index.html and getdates uodated @10:26 pm

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,7 +2,7 @@
 const currentYear = new Date().getFullYear();
 
 // Insertion dans le span
-document.getElementById("currentyear").textContent = currentYear;
+document.getElementById("currentyear").textContent = "Copyright " + currentYear;
 
 // Date de dernière modification
 document.getElementById("lastModified").textContent = "This page was last modified on: " + document.lastModified;
